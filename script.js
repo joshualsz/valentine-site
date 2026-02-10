32b56547431202c9f837ef3e8eb9d6c284607a7d
joshualsz <131629716+joshualsz@users.noreply.github.com>
2026-02-10 15:51:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const response = document.getElementById("response");
+
+yesBtn.addEventListener("click", () => {
+  response.innerHTML = "YAYYY 🥰 I love you ❤️";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * 200 - 100;
+  const y = Math.random() * 200 - 100;
+
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+});
+
+const heartsContainer = document.getElementById("hearts");
+
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+  
+    heart.innerHTML = "❤️";
+  
+    heart.style.left = Math.random() * 100 + "vw"; // random horizontal
+    heart.style.top = Math.random() * 100 + "vh";  // random vertical
+    heart.style.fontSize = (Math.random() * 20 + 15) + "px";
+    heart.style.animationDuration = (Math.random() * 4 + 4) + "s";
+  
+    heartsContainer.appendChild(heart);
+  
+    setTimeout(() => {
+      heart.remove();
+    }, 8000);
+  }
+  
+
+// spawn every 300ms
+setInterval(createHeart, 300);
